872cb364aaa821389966ebd567bdb2c1f2b9b793
amy-ym-yu <aymyu@ucdavis.edu>
2024-04-03 22:02:03 -0700

    updates

---
+++ next.config.js
@@ -0,0 +1,26 @@
+/** @type {import('next').NextConfig} */
+
+module.exports = {
+  async redirects() {
+    return [
+      {
+        source: "/about",
+        destination: "/about/ucd",
+        permanent: true,
+      },
+      {
+        source: "/events",
+        destination: "/events/chapter",
+        permanent: true,
+      },
+      {
+        source: "/members",
+        destination: "/members/new",
+        permanent: true,
+      },
+    ];
+  },
+  images: {
+    domains: ["res.cloudinary.com"],
+  },
+};
