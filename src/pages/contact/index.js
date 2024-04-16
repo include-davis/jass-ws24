ec97366403f28eda87bc58a9b257a7b87951a46e
Shota Ruo <scruo@ucdavis.edu>
2024-04-16 16:44:23 -0700

    basic text
    added basic texts and container

--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -1,6 +1,55 @@
+
+import styles from "@/styles/pages/contact/contact.module.scss"
+
+
 export default function Contact() {
-    return (
-      <h1>contact</h1>
-    );
-  }
+  return (
+    <><body id="whole">
+      <div className={styles.contactContainer}>
+      <div className={styles.leftSection}>
+      <div id="contact-us">
+        <h1 className={styles.contactTitle}>Get In Touch With Us!</h1>
+        <p className={styles.contactText}>Lorem ipsum dolor sit amet consectetur<br />
+          adipiscing elit, sed do eiusmod tempor<br />
+          incididunt ut labore et dolore magna aliqua.</p>
+      </div>
+      </div>
+      
+
+
+      <div className={styles.rightSection}>
+      <div id="container">
+        <h1 className={styles.contactForm}>Contact Form</h1>
+        <label htmlFor="name">Name</label>
+        <input type="text" id="name" name="name" />
+        
+        <label htmlFor="email">Email</label>
+        <input type="text" id="email" name="email" />
+        <br></br>
+
+        <label htmlFor="subject">Subject</label>
+        <input type="text" id="subject" name="subject" />
+        <br></br>
+
+        <label htmlFor="textbook">Textbook</label>
+        <input type="text" id="textbook" name="textbook" />
+
+
+
+
+
+      </div>
+      </div>
+
+      </div>
+
+    </body>
+
+
+
+    </>
+
+
+  );
+}
   
