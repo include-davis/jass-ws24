034f1ac04050c467080b6133732c13ca6a23e61b
Haylie Tan <141064325+haylietan@users.noreply.github.com>
2024-04-15 17:34:01 -0700

    Added text

--- src/pages/meetus/index.js
+++ src/pages/meetus/index.js
@@ -1,10 +1,18 @@
+import styles from './meetus.css';
+
 export default function MeetUs() {
     return (
       <div>
-        <div>
+        
+        <div className="intro">
           <h1>Meet Us</h1>
           <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
         </div>
+
+        <div>
+          <h1> Meet our Board Members </h1>
+        </div>
+
       </div>
     );
   }
