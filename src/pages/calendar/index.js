88dd7903949e454f6765ded852fb3c4391a65f89
Michelle <michellew.yeoh@gmail.com>
2024-04-15 17:59:30 -0700

    starting changes

--- src/pages/calendar/index.js
+++ src/pages/calendar/index.js
@@ -1,6 +1,13 @@
+import '@/styles/pages/calendar/calendar.module.scss';
+
 export default function Calendar() {
     return (
-      <h1>calendar</h1>
+      <div className="calandar-body">
+        <h1>Calendar</h1>
+        <p>View our calandar to see upcoming events!</p>
+        <div className="calandar">
+          {/* insert calandar here */}
+        </div>
+      </div>
     );
   }
-  
