724cc69438c5e6d3812a726f1db2b06ec3d4c09e
rachelhlin <racheljingo1102@gmail.com>
2024-04-17 15:43:14 -0700

    Working on newsletter
    Created email and submit button

--- src/pages/components/index.js
+++ src/pages/components/index.js
@@ -1,6 +1,33 @@
-export default function Components() {
+import React, { useState } from 'react';
+import styles from "@/styles/components/newsletter/newsletter.module.scss"
+
+const newsletter = () => {
+    const [email, setEmail] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log('Email submitted:', email);
+        setEmail('');
+    };
+
     return (
-      <h1>components</h1>
+        <section className={styles.container}>
+          <div className={styles.section}></div>
+            <h2 className={styles.text}>Sign up for our newsletter</h2>
+            <h2 className={styles.text}>& stay connected!</h2>
+            <form className={styles.form} onSubmit={handleSubmit}>
+                <input
+                    type="email"
+                    value={email}
+                    className={styles.input}
+                    onChange={(e) => setEmail(e.target.value)}
+                    placeholder="Enter email"
+                    required
+                />
+                <button type="submit" className={styles.button}>Submit</button>
+            </form>
+        </section>
     );
-  }
-  
+};
+
+export default newsletter;
