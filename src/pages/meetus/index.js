2296034fadb8b1e03e62a65db2adae16a76af094
Haylie Tan <141064325+haylietan@users.noreply.github.com>
2024-04-17 16:39:46 -0700

    Connected scss to index.js

--- src/pages/meetus/index.js
+++ src/pages/meetus/index.js
@@ -1,16 +1,15 @@
-import '@/styles/pages/meetus.module.scss';
-//mport styles from '@/styles/pages/meetus/module.scss';
+import styles from '@/styles/pages/meetus/meetus.module.scss';
 
 export default function MeetUs() {
     return (
       <div>
         
-        <div className="intro">
+        <div className={styles.intro}>
           <h1>Meet Us</h1>
           <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
         </div>
 
-        <div>
+        <div className={styles.board}>
           <h1> Meet our Board Members </h1>
         </div>
 
