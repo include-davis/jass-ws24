4d96df68f06133adad1e3c672e63ce423a45702f
tylguo <tyleguo2004@gmail.com>
2024-04-17 16:53:09 -0700

    Updated JoinUs  to use card component with props for title and description

--- src/pages/joinus/index.js
+++ src/pages/joinus/index.js
@@ -1,53 +1,43 @@
 import styles from "@/styles/pages/joinus/joinus.module.scss";
 
 
-const Card1 = () => (
-    <div className={styles.card}>
-      <div className={styles.innerBox}></div>
-      <div className={styles.cardContent}>
-        <p className={styles.cardTitle}>General Meetings</p>
-        <p className={styles.cardDescription}>Every Thursday at 7pm we have fun activities, slices of Japanese culture, and information about upcoming events!</p>
-      </div>
+const Card = ({ title, description }) => (
+  <div className={styles.card}>
+    <div className={styles.innerBox}></div>
+    <div className={styles.cardContent}>
+      <p className={styles.cardTitle}>{title}</p>
+      <p className={styles.cardDescription}>{description}</p>
     </div>
-  );
+  </div>
+);
+
   
-  const Card2 = () => (
-    <div className={styles.card}>
-      <div className={styles.innerBox}></div>
-      <div className={styles.cardContent}>
-        <p className={styles.cardTitle}>Service & Social Events</p>
-        <p className={styles.cardDescription}>Find opportunities to help the community as well as taking part in fun group events!</p>
+export default function JoinUs() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.cardContainer}>
+        <p className={styles.title}>Join us for our...</p>
       </div>
-    </div>
-  );
-  
-  const Card3 = () => (
-    <div className={styles.card}>
-      <div className={styles.innerBox}></div>
-      <div className={styles.cardContent}>
-        <p className={styles.cardTitle}>Big/Little System</p>
-        <p className={styles.cardDescription}>Take part in our Big/Little program and Family System to create even closer friendships within the club!</p>
-      </div>
-    </div>
-  );
-  
-  export default function JoinUs() {
-    return (
-      <div className={styles.container}>
-        <div className={styles.cardContainer}>
-          <p className={styles.title}>Join us for our...</p>
+      <div className={styles.cardContainer}>
+        <div className={styles.cardWrapper}>
+          <Card
+            title="General Meetings"
+            description="Every Thursday at 7pm we have fun activities, slices of Japanese culture, and information about upcoming events!"
+          />
         </div>
-        <div className={styles.cardContainer}>
-          <div className={styles.cardWrapper}>
-            <Card1 />
-          </div>
-          <div className={styles.cardWrapper}>
-            <Card2 />
-          </div>
-          <div className={styles.cardWrapper}>
-            <Card3 />
-          </div>
+        <div className={styles.cardWrapper}>
+          <Card
+            title="Service & Social Events"
+            description="Find opportunities to help the community as well as taking part in fun group events!"
+          />
+        </div>
+        <div className={styles.cardWrapper}>
+          <Card
+            title="Big/Little System"
+            description="Take part in our Big/Little program and Family System to create even closer friendships within the club!"
+          />
         </div>
       </div>
-    );
-  }
+    </div>
+  );
+}
