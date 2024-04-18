8f58c661a3e8c057ea915b0b82f5b7a0458c7d5c
Michelle <michellew.yeoh@gmail.com>
2024-04-17 17:01:07 -0700

    completed calendar layout

--- src/pages/calendar/index.js
+++ src/pages/calendar/index.js
@@ -1,13 +1,20 @@
-import '@/styles/pages/calendar/calendar.module.scss';
+import styles from '@/styles/pages/calendar/calendar.module.scss';
 
 export default function Calendar() {
     return (
-      <div className="calandar-body">
-        <h1>Calendar</h1>
-        <p>View our calandar to see upcoming events!</p>
-        <div className="calandar">
-          {/* insert calandar here */}
+        <div className={styles.gaegu_regular}>
+            <style>
+                @import
+                url('https://fonts.googleapis.com/css2?family=Gaegu&display=swap');
+            </style>
+            <div className={styles.calendar_body}>
+                <h1>Calendar</h1>
+                <br />
+                <p>View our calandar to see upcoming events!</p>
+                <div className={styles.calendar}>
+                    {/* insert calandar here */}
+                </div>
+            </div>
         </div>
-      </div>
     );
-  }
+}
