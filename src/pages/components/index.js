ade0c89f06bf26a698508de9da0afea4292026f4
Michelle <michellew.yeoh@gmail.com>
2024-04-17 18:04:48 -0700

    layout without photo

--- src/pages/components/index.js
+++ src/pages/components/index.js
@@ -1,11 +1,26 @@
+import styles from '@/styles/components/components.module.scss';
+import Video from './Video.png';
+
 export default function Components() {
     return (
-        <div>
-            <h1>JASS @ UC Davis</h1>
-            <p>
-                Meet friendly and open people through events, games, and more!
-            </p>
-            <button>Join JASS</button>
+        <div className={styles.gaegu_regular}>
+            <style>
+                @import
+                url('https://fonts.googleapis.com/css2?family=Gaegu&display=swap');
+            </style>
+            <div className={styles.landing_body}>
+                <img src={Video} alt="group_photo" />
+                <div className={styles.landing_center}>
+                    <h1>JASS @ UC Davis</h1>
+                    <br />
+                    <p>
+                        Meet friendly and open people through events, games, and
+                        more!
+                    </p>
+                    <br />
+                    <button>Join JASS</button>
+                </div>
+            </div>
         </div>
     );
 }
