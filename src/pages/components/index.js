819e76a19c3eabe8bd1b3c7af3ba2fd43601eab3
Shota Ruo <scruo@ucdavis.edu>
2024-04-28 23:25:48 -0700

    added css sizes for other mobile devicees

--- src/pages/components/index.js
+++ src/pages/components/index.js
@@ -1,6 +1,10 @@
+import { Contact } from '@/components/contact'
 export default function Components() {
     return (
-      <h1>components</h1>
+      <>
+        <h1>components</h1>
+        <Contact/>
+      </>
     );
   }
   
--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -4,70 +4,8 @@ import styles from "@/styles/pages/contact/contact.module.scss"
 
 export default function Contact() {
   return (
-    <><div className={styles.main}>
-        <div className={styles.contactContainer}>
-          <div className={styles.leftSection}>
-            <div id="contact-us">
-              <h1 className={styles.contactTitle}>Get In Touch With Us!</h1>
-                <p className={styles.contactText}>Lorem ipsum dolor sit amet consectetur<br />
-                  adipiscing elit, sed do eiusmod tempor<br />
-                  incididunt ut labore et dolore magna aliqua.</p>
-            </div>
-          </div>
-      
-
-
-      <div className={styles.rightSection}>
-        <div id="container">
-          <h1 className={styles.contactForm}>Contact Form</h1>
-
-            <div className={styles.inputRow}>
-              <div className={styles.inputContainer}>
-                <label htmlFor="name" className={styles.label}>Name</label>
-        
-                <input type="text" id="name" name="name"  className={`${styles.inputText} ${styles.nameInput}`} />
-              </div>
-
-
-
-        <div className={styles.inputContainer}>
-        <label htmlFor="email" className={styles.label}>Email</label>
-        <input type="text" id="email" name="email"  className={`${styles.inputText} ${styles.emailInput}`} />
-        </div>
-        </div>
-
-
-
-        <br></br>
-        <div className={styles.inputContainer}>
-          <label htmlFor="subject" className={styles.label}>Subject</label>
-          <input type="text" id="subject" name="subject" className={`${styles.inputText} ${styles.subjectInput}`} />
-        </div>
-        <br></br>
-
-        <div className={styles.inputContainer}>
-          <label htmlFor="textbook" className={styles.label}>Textbook</label>
-           {/*ensure text */}
-          {/*<input type="text" id="textbook" name="textbook" className={`${styles.inputText} ${styles.textbookInput}`} /> */}
-          <textarea style={{ verticalAlign: 'top' }} id="textbook" name="textbook" className={`${styles.inputText} ${styles.textbookInput}`} />
-        </div>
-
-
-
-
-
-      </div>
-      </div>
-
-      </div>
-
-    </div>
-
-
-
+    <>
     </>
-
-
   );
 }
   
