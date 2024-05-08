f5461eea84ee03e59ccddb9ce67ca4f250298600
Michelle <michellew.yeoh@gmail.com>
2024-05-08 16:25:06 -0700

    JSON

--- src/components/calendar.js
+++ src/components/calendar.js
@@ -1,5 +1,8 @@
 import styles from '@/styles/pages/calendar/calendar.module.scss';
 
+const content = {
+    heading1: 'View our calandar to see upcoming events!',
+};
 export function CalendarComp() {
     return (
         <div className={styles.gaegu_regular}>
@@ -10,7 +13,7 @@ export function CalendarComp() {
             <div className={styles.calendar_body}>
                 <h1>Calendar</h1>
                 <br />
-                <p>View our calandar to see upcoming events!</p>
+                <p>{content.heading1}</p>
                 <div className={styles.calendar}>
                     {/* insert calandar here */}
                 </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,7 @@
-import { Component } from 'react';
-import newsletter from './components/newsletter';
-
 export default function Home() {
-    return(
+    return (
         <div>
             <h1>Hello World</h1>
-            <newsletter />
         </div>
     );
 }
