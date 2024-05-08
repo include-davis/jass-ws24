333546553c4f3c74bfe9d8bb0c1020fe8b2a53ec
Shota Ruo <scruo@ucdavis.edu>
2024-05-07 17:40:26 -0700

    added space between the two input boxes

--- src/components/contact.js
+++ src/components/contact.js
@@ -4,7 +4,8 @@ import { useState } from 'react'
 
 //i can create my own styles for the submit button 
 //I need to also add colors it doesnt matter too much
-//
+
+//create a little more space between name and email text boxes. increase margin - done
 
 //create json objects for texts on left side  - done
 
@@ -14,76 +15,76 @@ const textContent = {
         description: "Lorem ipsum dolor sit amet consectur adipiscing elitm sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
 
     }
-   
+
 }
-export function Contact () {
+export function Contact() {
     return (
         <>
             <div className={styles.main}>
-            <div className={styles.contactContainer}>
-            <div className={styles.leftSection}>
-                <div id="contact-us">
-                <h1 className={styles.contactTitle}>Get In Touch With Us!</h1>
-                    <p className={styles.contactText}>Lorem ipsum dolor sit amet consectetur<br />
-                    adipiscing elit, sed do eiusmod tempor<br />
-                    incididunt ut labore et dolore magna aliqua.</p>
+                <div className={styles.contactContainer}>
+                    <div className={styles.leftSection}>
+                        <div id="contact-us">
+                            <h1 className={styles.contactTitle}>Get In Touch With Us!</h1>
+                            <p className={styles.contactText}>Lorem ipsum dolor sit amet consectetur<br />
+                                adipiscing elit, sed do eiusmod tempor<br />
+                                incididunt ut labore et dolore magna aliqua.</p>
+                        </div>
+                    </div>
+
+
+
+                    <div className={styles.rightSection}>
+                        <div id="container">
+                            <h1 className={styles.contactForm}>Contact Form</h1>
+
+                            <div className={styles.inputRow}>
+                                <div className={styles.inputContainer}>
+                                    <label htmlFor="name" className={styles.label}>Name</label>
+
+                                    <input type="text" id="name" name="name" className={`${styles.inputText} ${styles.nameInput}`} />
+                                </div>
+
+
+
+                                <div className={styles.inputContainer}>
+                                    <label htmlFor="email" className={styles.label}>Email</label>
+                                    <input type="text" id="email" name="email" className={`${styles.inputText} ${styles.emailInput}`} />
+                                </div>
+                            </div>
+
+
+
+                            <br></br>
+                            <div className={styles.inputContainer}>
+                                <label htmlFor="subject" className={styles.label}>Subject</label>
+                                <input type="text" id="subject" name="subject" className={`${styles.inputText} ${styles.subjectInput}`} />
+                            </div>
+                            <br></br>
+
+                            <div className={styles.inputContainer}>
+                                <label htmlFor="textbook" className={styles.label}>Textbook</label>
+                                {/*ensure text */}
+                                {/*<input type="text" id="textbook" name="textbook" className={`${styles.inputText} ${styles.textbookInput}`} /> */}
+                                <textarea style={{ verticalAlign: 'top' }} id="textbook" name="textbook" className={`${styles.inputText} ${styles.textbookInput}`} />
+                            </div>
+
+                            <div className={styles.submitButton}>
+                                <button type="submit" id="submitButton" name="submit">Submit</button>
+                            </div>
+
+
+
+
+
+                        </div>
+                    </div>
+
                 </div>
-            </div>
-        
 
-
-        <div className={styles.rightSection}>
-            <div id="container">
-            <h1 className={styles.contactForm}>Contact Form</h1>
-
-                <div className={styles.inputRow}>
-                <div className={styles.inputContainer}>
-                    <label htmlFor="name" className={styles.label}>Name</label>
-            
-                    <input type="text" id="name" name="name"  className={`${styles.inputText} ${styles.nameInput}`} />
-                </div>
-
-
-
-            <div className={styles.inputContainer}>
-            <label htmlFor="email" className={styles.label}>Email</label>
-            <input type="text" id="email" name="email"  className={`${styles.inputText} ${styles.emailInput}`} />
-            </div>
             </div>
 
 
 
-            <br></br>
-            <div className={styles.inputContainer}>
-            <label htmlFor="subject" className={styles.label}>Subject</label>
-            <input type="text" id="subject" name="subject" className={`${styles.inputText} ${styles.subjectInput}`} />
-            </div>
-            <br></br>
-
-            <div className={styles.inputContainer}>
-            <label htmlFor="textbook" className={styles.label}>Textbook</label>
-            {/*ensure text */}
-            {/*<input type="text" id="textbook" name="textbook" className={`${styles.inputText} ${styles.textbookInput}`} /> */}
-            <textarea style={{ verticalAlign: 'top' }} id="textbook" name="textbook" className={`${styles.inputText} ${styles.textbookInput}`} />
-            </div>
-
-            <div className={styles.submitButton}>
-                <button type="submit">Submit</button>
-            </div>
-
-
-
-
-
-        </div>
-        </div>
-
-        </div>
-
-        </div>
-
-
-
-    </>
+        </>
     );
 }
