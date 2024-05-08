46af47c7f6b604dcc96135f59ce30615c58c0acb
amy-ym-yu <aymyu@ucdavis.edu>
2024-05-08 16:43:34 -0700

    update cal page

--- src/pages/calendar/index.js
+++ src/pages/calendar/index.js
@@ -1,20 +1,5 @@
 import styles from '@/styles/pages/calendar/calendar.module.scss';
 
 export default function Calendar() {
-    return (
-        <div className={styles.gaegu_regular}>
-            <style>
-                @import
-                url('https://fonts.googleapis.com/css2?family=Gaegu&display=swap');
-            </style>
-            <div className={styles.calendar_body}>
-                <h1>Calendar</h1>
-                <br />
-                <p>View our calandar to see upcoming events!</p>
-                <div className={styles.calendar}>
-                    {/* insert calandar here */}
-                </div>
-            </div>
-        </div>
-    );
+    return <></>;
 }
