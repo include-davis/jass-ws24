9c5b8a6c3e441ef458787fd68784677d874da950
amy-ym-yu <aymyu@ucdavis.edu>
2024-05-08 16:48:34 -0700

    style

--- src/components/calendar.js
+++ src/components/calendar.js
@@ -6,10 +6,6 @@ const content = {
 export function CalendarComp() {
     return (
         <div className={styles.gaegu_regular}>
-            <style>
-                @import
-                url('https://fonts.googleapis.com/css2?family=Gaegu&display=swap');
-            </style>
             <div className={styles.calendar_body}>
                 <h1>Calendar</h1>
                 <br />
