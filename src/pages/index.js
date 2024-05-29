b4775ab773564f3500ff609fd06a3040be14023d
Michelle <michellew.yeoh@gmail.com>
2024-05-29 16:30:07 -0700

    add landing to main page

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,11 +2,13 @@ import { Footer } from '@/components/footer';
 import { EventsSlider } from '@/components/eventsSlider';
 import { Newsletter } from '@/components/newsletter';
 import Header from '@/components/header';
+import Landing from '@/components/landing';
 
 export default function Home() {
     return (
         <div>
             <Header />
+            <Landing />
             <Newsletter />
             <EventsSlider />
             <Footer />
