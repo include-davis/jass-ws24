4eff2e7dcf3bcae9e04bcfebb013b765264ab319
amy-ym-yu <amy.ym.yu3@gmail.com>
2024-06-11 16:58:53 -0700

    update component

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,12 +3,14 @@ import { EventsSlider } from '@/components/eventsSlider';
 import { Newsletter } from '@/components/newsletter';
 import { Header } from '@/components/header';
 import { Landing } from '@/components/landing';
+import WhoWeAre from '@/components/whoWeAre';
 
 export default function Home() {
     return (
         <div>
             <Header />
             <Landing />
+            <WhoWeAre />
             <Newsletter />
             <EventsSlider />
             <Footer />
