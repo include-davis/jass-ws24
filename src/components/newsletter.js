310560360a807ec1f83ce00ea06deacc3e72db0d
amy-ym-yu <amy.ym.yu3@gmail.com>
2024-06-11 13:10:42 -0700

    updates

--- src/components/newsletter.js
+++ src/components/newsletter.js
@@ -1,13 +1,13 @@
 import React, { useState } from 'react';
-import styles from "@/styles/components/newsletter/newsletter.module.scss"
-import Image from 'next/image'
+import styles from '@/styles/components/newsletter/newsletter.module.scss';
+import Image from 'next/image';
 
 const images = {
     image1: '/images/JASSNewsletter1.png',
     image2: '/images/JASSNewsletter2.png',
 };
 
-export function Newsletter(){
+export function Newsletter() {
     const [email, setEmail] = useState('');
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -18,10 +18,20 @@ export function Newsletter(){
     return (
         <section className={styles.container}>
             <div className={styles.seccontainer}>
-                <h1 className={styles.text}>Sign up for our newsletter & stay connected!</h1>
+                <h1 className={styles.text}>
+                    Sign up for our newsletter & stay connected!
+                </h1>
                 <div className={styles.imagecontainermobile}>
-                    <img src={images.image1} alt="Image 1" className={styles.image} />
-                    <img src={images.image2} alt="Image 2" className={styles.image} />
+                    <img
+                        src={images.image1}
+                        alt="Image 1"
+                        className={styles.image}
+                    />
+                    <img
+                        src={images.image2}
+                        alt="Image 2"
+                        className={styles.image}
+                    />
                 </div>
                 <form className={styles.form} onSubmit={handleSubmit}>
                     <input
@@ -32,18 +42,27 @@ export function Newsletter(){
                         placeholder="Enter email"
                         required
                     />
-                    
-                    <button type="submit" className={styles.button}>Submit</button>
-                    
+
+                    <button type="submit" className={styles.button}>
+                        Submit
+                    </button>
                 </form>
             </div>
 
             <div className={styles.imagecontainer}>
-                <img src={images.image1} alt="Image 1" className={styles.image} />
-                <img src={images.image2} alt="Image 2" className={styles.image} />
+                <img
+                    src={images.image1}
+                    alt="Image 1"
+                    className={styles.image}
+                />
+                <img
+                    src={images.image2}
+                    alt="Image 2"
+                    className={styles.image}
+                />
             </div>
         </section>
     );
-};
+}
 
 export default Newsletter;
