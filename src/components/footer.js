c284ab8a5cb0f3f41da95a54dbfbb5f926afdd14
amy-ym-yu <amy.ym.yu3@gmail.com>
2024-07-07 20:05:06 -0700

    attempt to fix footer for sm-desktop -- needs more work

--- src/components/footer.js
+++ src/components/footer.js
@@ -13,22 +13,6 @@ export function Footer() {
         <div className={styles.footer}>
             <div className={styles.footerTop}>
                 <div className={styles.footerContent}>
-                    <div className={styles.linksOuterFrame}>
-                        <div className={styles.linksInnerFrame}>
-                            <div className={styles.linksLeft}>
-                                <p className={styles.link}>Home</p>
-                                <p className={styles.link}>Calendar</p>
-                                <p className={styles.link}>Meet Us</p>
-                                <p className={styles.link}>Join Our Cabinet</p>
-                            </div>
-                            <div className={styles.linksRight}>
-                                <p className={styles.link}>Media</p>
-                                <p className={styles.link}>Youtube</p>
-                                <p className={styles.link}>Google Photos</p>
-                            </div>
-                        </div>
-                    </div>
-
                     <div className={styles.contact}>
                         <img
                             className={styles.image}
@@ -72,21 +56,38 @@ export function Footer() {
 
                         {/* Add social media icons here */}
                     </div>
-                </div>
-
-                <div className={styles.newsletter}>
-                    <p className={styles.title}>
-                        Sign up for our newsletter & stay connected!
-                    </p>
-                    <div className={styles.interactive}>
-                        <div className={styles.inputWrapper}>
-                            <input
-                                className={styles.input}
-                                type="email"
-                                placeholder="Enter email"
-                            />
+                    <div className={styles.linksOuterFrame}>
+                        <div className={styles.linksInnerFrame}>
+                            <div className={styles.linksLeft}>
+                                <p className={styles.link}>Home</p>
+                                <p className={styles.link}>Calendar</p>
+                                <p className={styles.link}>Meet Us</p>
+                                <p className={styles.link}>Join Our Cabinet</p>
+                            </div>
+                            <div className={styles.linksRight}>
+                                <p className={styles.link}>Media</p>
+                                <p className={styles.link}>Youtube</p>
+                                <p className={styles.link}>Google Photos</p>
+                            </div>
                         </div>
-                        <button className={styles.submitButton}>Submit</button>
+                    </div>
+
+                    <div className={styles.newsletter}>
+                        <p className={styles.title}>
+                            Sign up for our newsletter & stay connected!
+                        </p>
+                        <div className={styles.interactive}>
+                            <div className={styles.inputWrapper}>
+                                <input
+                                    className={styles.input}
+                                    type="email"
+                                    placeholder="Enter email"
+                                />
+                            </div>
+                            <button className={styles.submitButton}>
+                                Submit
+                            </button>
+                        </div>
                     </div>
                 </div>
             </div>
