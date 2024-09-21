459932070ab63391c639b0cd6f6ca68c00c67bb4
Austin Shih <austinhshih@gmail.com>
2024-09-21 00:49:17 -0700

    fixed font family added metadata

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,12 +1,34 @@
 import '../styles/globals.scss';
-import Layout from '@/components/layout';
+import Head from 'next/head';
+import Navbar from '../components/header';
+import Footer from '../components/footer';
+import { Gaegu } from 'next/font/google';
+
+const gaegu = Gaegu({
+    subsets: ['latin'],
+    variable: '--font-gaegu',
+    weight: ['300', '400', '700'],
+});
 
 export default function MyApp({ Component, pageProps }) {
     return (
         <>
-            <Layout>
+            <Head>
+                <title>{`JASS UCD`}</title>
+                <meta
+                    name="description"
+                    content="​The Japanese American Student Society or JASS (est. 1982) is a student organization at the University of California, Davis with an eye to service, social, and cultural happenings."
+                />
+                <meta
+                    name="keywords"
+                    content="ucd, ucdavis, davis, japanese, japanese american, students, community, jass, jassucd, student society, culture, service, social"
+                />
+            </Head>
+            <main className={gaegu.className}>
+                <Navbar />
                 <Component {...pageProps} />
-            </Layout>
+                <Footer />
+            </main>
         </>
     );
 }
